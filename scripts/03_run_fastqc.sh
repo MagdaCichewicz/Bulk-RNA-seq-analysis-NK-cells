78a6978319d3c7ddb217ffc18496ee170f462163
MagdaCichewicz <magdalena.cichewicz.bio@gmail.com>
2026-09-03 14:07:57 -0700

    Update 03_run_fastqc.sh

--- scripts/03_run_fastqc.sh
+++ scripts/03_run_fastqc.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -euo pipefail
 
+FASTQ_DIR="/mnt/disk1/bulk_rnaseq/fastq"
+FASTQC_DIR="/mnt/disk1/bulk_rnaseq/fastqc"
+
+mkdir -p "$FASTQC_DIR"
+
+fastqc \
+    "$FASTQ_DIR"/*.fastq.gz \
+    --outdir "$FASTQC_DIR" \
+    --threads 8
