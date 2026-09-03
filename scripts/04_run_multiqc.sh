68e6d8d0625e51544eb0219917d58c03a99e0178
MagdaCichewicz <magdalena.cichewicz.bio@gmail.com>
2026-09-03 14:08:43 -0700

    Update 04_run_multiqc.sh

--- scripts/04_run_multiqc.sh
+++ scripts/04_run_multiqc.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -euo pipefail
 
+FASTQC_DIR="/mnt/disk1/bulk_rnaseq/fastqc"
+RESULTS_DIR="/mnt/disk1/bulk_rnaseq/results"
+
+mkdir -p "$RESULTS_DIR"
+
+multiqc \
+    "$FASTQC_DIR" \
+    --outdir "$RESULTS_DIR"
